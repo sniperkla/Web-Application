0859125adeef81059a5a83693d6a7ce0ab98d1c4
staff <staff>
2017-05-02 16:16:59 +0700

    fisrt

--- post.js
+++ post.js
@@ -8,7 +8,6 @@ var posts = [{'id':0,'timage':'https://scontent.fbkk5-2.fna.fbcdn.net/v/t31.0-8/
 			 {'id':2,'timage':'https://scontent.fbkk5-2.fna.fbcdn.net/v/t1.0-9/1236263_491911204244005_660416981911721022_n.jpg?oh=e38da3bb74b94d7e197a12d108eaa817&oe=597F5715','name':'Katanyoo','comment':'การเรียนอ่ะเรื่องจิ๊บๆ ที่เห็นเลือดซิบๆอ่ะนั่นหนามบาด💓💓'}]
 var postIndex = 3;
 app.use(express.static('public')) 
-
 router.route('/posts') 
 	.get( function(req,res) {
 		res.json(posts);
@@ -50,6 +49,6 @@ router.route('/posts/:post_id')
 app.use('/api', bodyParser.json(), router)
 // app.use('/api',bodyParser.urlencoded({extended:false}), router)
 
-app.listen(50038, function() {
+app.listen(50091, function() {
 	console.log('server is running...')
 })
